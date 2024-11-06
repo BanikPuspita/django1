ad47b4290f562857cd4d6c5d91d6ac0c724ab510
puspita <puspitabanik213@gmail.com>
2024-11-06 23:32:34 +0600

    updated

--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app_F/', include('app_F.urls')), # added app_F app
 ]
+
